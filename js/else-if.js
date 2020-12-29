a08f3ae35cae97216a30eaaaa4813de037b3b810
Fsesnick <fernandasesnick@gmail.com>
2020-12-29 15:35:58 +0000

    multiple statements

---
+++ js/else-if.js
@@ -0,0 +1,11 @@
+const weather = '';
+
+if(weather === 'sun'){
+  console.log("It is sunny, so I am going swimming");
+}else if(weather === 'rain'){
+  console.log("It is raining, I will read a book.");  
+}else if(weather ==='snow'){
+console.log("It's snowing, I am going sledding.");
+}else{
+  console.log("I don't know what I'm doing today");
+}
